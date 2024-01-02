3172df1f66b1f35fc01894ea0cc9e4e9ec1b7e5d
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-01-02 10:46:26 -0300

    fix icons properties

--- app/ui/svgs/PictorialMark.tsx
+++ app/ui/svgs/PictorialMark.tsx
@@ -17,11 +17,11 @@ export default function PictorialMark({
       <g id="Layer_1-2" data-name="Layer 1">
         <path
           d="M23,14.6H3c-1.65,0-3-1.35-3-3V3C0,1.35,1.35,0,3,0h20c1.65,0,3,1.35,3,3v8.6c0,1.65-1.35,3-3,3ZM3,2c-.55,0-1,.45-1,1v8.6c0,.55.45,1,1,1h20c.55,0,1-.45,1-1V3c0-.55-.45-1-1-1H3Z"
-          stroke-width="0"
+          strokeWidth="0"
         />
         <path
           d="M6.92,11.7h-3c-.55,0-1-.45-1-1s.45-1,1-1h3c.55,0,1,.45,1,1s-.45,1-1,1Z"
-          stroke-width="0"
+          strokeWidth="0"
         />
       </g>
     </svg>
