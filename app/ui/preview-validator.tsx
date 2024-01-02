029b95393a94e33dab4ce202c7d4af64d62ee71c
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-01-02 20:54:02 -0300

    add discord preview & fixes

--- app/ui/preview-validator.tsx
+++ app/ui/preview-validator.tsx
@@ -57,7 +57,7 @@ export default function PreviewValidator({
     const normalizedUrl = getUrlFromString(eventUrl)
 
     if (!!normalizedUrl) {
-      router.replace(`${eventUrl ? `/validator?url=${eventUrl}` : ''}`)
+      router.push(`${eventUrl ? `/validator?url=${eventUrl}` : ''}`)
     } else {
       setInputError(true)
       console.log('error')
@@ -203,7 +203,12 @@ export default function PreviewValidator({
           <div className="flex flex-col items-center justify-center gap-4">
             <TypographyH2 className="w-full">Previews</TypographyH2>
             {metatags && metatags['og:image'] && (
-              <div className="grid grid-flow-col items-start justify-center gap-8 xl:grid-cols-4 xl:gap-4">
+              <div
+                className="grid grid-flow-row items-start justify-center gap-8 xl:gap-4"
+                style={{
+                  gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+                }}
+              >
                 {/* Twitter/X */}
                 <div className="flex w-full flex-col items-center justify-center gap-4">
                   <div className="flex items-center justify-center gap-4">
@@ -246,14 +251,14 @@ export default function PreviewValidator({
                     <TypographyH3 className="">Facebook</TypographyH3>
                   </div>
 
-                  <div className="relative font-['Helvetica']">
+                  <div className="relative font-['Helvetica'] shadow-[0_1px_2px_rgb(0_0_0_/_0.2)]">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
                       src={metatags['og:image']}
                       alt="Preview"
                       className="aspect-[1.91/1] w-full object-cover"
                     />
-                    <div className="flex flex-col items-start justify-center border-b border-[#dddfe2] bg-[#EBECED] px-3 py-2.5 dark:border-[#3e4042] dark:bg-[#3a3b3c]">
+                    <div className="flex flex-col items-start justify-center bg-[#EBECED] px-3 py-2.5 dark:bg-[#3a3b3c]">
                       <span className="text-left text-xs font-normal uppercase leading-[11px] text-[#606770] dark:text-[#b0b3b8]">
                         {getDomainWithoutWWW(normalizedUrl ?? '')}
                       </span>
@@ -343,6 +348,42 @@ export default function PreviewValidator({
                         className="aspect-[1.91/1] h-full w-full max-w-[360px] cursor-zoom-in rounded-[8px] bg-cover bg-center bg-no-repeat shadow-[inset_0_0_0_1px_rgba(0,0,0,0.1)]"
                       />
                     </div>
+                  </div>
+                </div>
+                {/* Discord */}
+                <div className="flex w-full flex-col items-center justify-center gap-4">
+                  <div className="flex items-center justify-center gap-4">
+                    <Image
+                      src="/social-icons/discord-icon.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className="h-6 w-6 dark:invert"
+                    />
+                    <TypographyH3 className="">Discord</TypographyH3>
+                  </div>
+                  <div className="flex w-full max-w-[432px] flex-col rounded-[4px] border-l-4 border-[#e3e5e8] bg-[#f2f3f5] pb-4 pl-3 pr-4 pt-2 font-[Noto_Sans] dark:border-[#1e1f22] dark:bg-[#2b2d31]">
+                    {metatags['og:site_name'] && (
+                      <span className="mt-2 text-xs font-normal text-[#313338] dark:text-[#dbdee1]">
+                        {metatags['og:site_name']}
+                      </span>
+                    )}
+                    <span className="mt-2 cursor-pointer break-words text-base font-semibold leading-[22px] text-[#006ce7] hover:underline dark:text-[#00a8fc]">
+                      {metatags['og:title'] ||
+                        metatags['twitter:title'] ||
+                        metatags.title}
+                    </span>
+                    <span className="mt-2 whitespace-pre-line text-sm leading-[18px] text-[#313338] dark:text-[#dbdee1]">
+                      {metatags['og:description'] ||
+                        metatags['twitter:description'] ||
+                        metatags.description}
+                    </span>
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={metatags['og:image']}
+                      alt="Preview"
+                      className="mt-3 aspect-[1.91/1] w-full cursor-pointer rounded-[4px] object-cover"
+                    />
                   </div>
                 </div>
               </div>
