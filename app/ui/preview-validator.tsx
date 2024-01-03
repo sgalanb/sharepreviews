10ddc02dd3e4d86b59ad970717954047a4383fc7
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-01-03 00:07:49 -0300

    add whatsapp preview

--- app/ui/preview-validator.tsx
+++ app/ui/preview-validator.tsx
@@ -398,11 +398,16 @@ export default function PreviewValidator({
                     />
                     <TypographyH3 className="">Telegram</TypographyH3>
                   </div>
-                  <div className="flex w-full max-w-[451px] flex-col rounded-[15px] bg-[#e3fee0] px-2 pb-[5px] pt-1 font-[Roboto] antialiased dark:bg-[#8774e1]">
+                  <div
+                    className="flex w-full max-w-[451px] flex-col rounded-[15px] bg-[#e3fee0] px-2 pb-[5px] pt-1 font-[Roboto] antialiased dark:bg-[#8774e1]"
+                    style={{
+                      boxShadow: 'rgba(16, 35, 47, 0.15) 0px 1px 2px 0px',
+                    }}
+                  >
                     <span className="cursor-pointer overflow-clip break-words text-base font-normal leading-[21px] text-[#5ca853] underline dark:text-white">
                       {normalizedUrl}
                     </span>
-                    <div className="my-1 flex w-full cursor-pointer flex-col rounded-[4px] border-l-[3px] border-[#5ca853] bg-[#5ca8531a] py-1 pl-1.5 pr-1.5 hover:bg-[rgba(92,168,83,0.2)] dark:border-white dark:bg-[#ffffff1a]">
+                    <div className="my-1 flex w-full cursor-pointer flex-col rounded-[4px] border-l-[3px] border-[#5ca853] bg-[#5ca8531a] py-1 pl-1.5 pr-1.5 hover:bg-[rgba(92,168,83,0.2)] dark:border-white dark:bg-[#ffffff1a] dark:hover:bg-[rgba(255,255,255,0.2)]">
                       {metatags['og:site_name'] && (
                         <span className="text-sm font-medium leading-[18px] text-[#5ca853] hover:underline dark:text-white">
                           {metatags['og:site_name']}
@@ -425,6 +430,50 @@ export default function PreviewValidator({
                         className="my-[3px] aspect-[1.91/1] w-full cursor-pointer rounded-[4px] object-cover"
                       />
                     </div>
+                  </div>
+                </div>
+                {/* WhatsApp */}
+                <div className="flex w-full flex-col items-center justify-center gap-4">
+                  <div className="flex items-center justify-center gap-4">
+                    <Image
+                      src="/social-icons/whatsapp-icon.svg"
+                      alt=""
+                      width={24}
+                      height={24}
+                      className="h-6 w-6 dark:invert"
+                    />
+                    <TypographyH3 className="">WhatsApp</TypographyH3>
+                  </div>
+                  <div
+                    className="flex w-full max-w-[336px] flex-col rounded-[7.5px] bg-[#d9fdd3] px-1 pb-2 pt-1  dark:bg-[#005c4b]"
+                    style={{
+                      boxShadow: 'rgba(11, 20, 26, 0.13) 0px 1px 0.5px 0px',
+                    }}
+                  >
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={metatags['og:image']}
+                      alt="Preview"
+                      className="aspect-[1.91/1] w-full cursor-pointer rounded-t-[6px] object-cover"
+                    />
+                    <div className="mb-1.5 flex w-full cursor-pointer flex-col rounded-[4px] bg-[#d1f4cc] px-2.5 py-1.5 dark:bg-[#025144]">
+                      <span className="mb-0.5 line-clamp-2 cursor-pointer text-ellipsis break-words text-[13px] font-normal leading-[19px] text-black dark:text-[#e9edefe0]">
+                        {metatags['og:title'] ||
+                          metatags['twitter:title'] ||
+                          metatags.title}
+                      </span>
+                      <span className="whitespace-pre-line text-xs font-normal leading-[19px] text-black dark:text-[#e9edefe0]">
+                        {metatags['og:description'] ||
+                          metatags['twitter:description'] ||
+                          metatags.description}
+                      </span>
+                      <span className="pt-[1px] text-xs font-normal leading-[19px] text-[#111b214d] hover:underline dark:text-[#e9edef4d]">
+                        {getDomainWithoutWWW(normalizedUrl ?? '')}
+                      </span>
+                    </div>
+                    <span className="cursor-pointer overflow-clip break-words px-1 text-[14px] font-normal leading-[19px] text-[#027eb5] underline dark:text-[#53bdeb]">
+                      {normalizedUrl}
+                    </span>
                   </div>
                 </div>
               </div>
