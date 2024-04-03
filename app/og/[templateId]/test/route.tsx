c7ceebb05afb2afc85f4a8aad6d2faccda7cb49b
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-04-03 20:59:25 -0300

    fix

--- app/og/[templateId]/test/route.tsx
+++ app/og/[templateId]/test/route.tsx
@@ -90,7 +90,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Thin',
           data: await fetch(
-            new URL('../../../../assets/inter-thin.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/214b8ddf-2967-43c2-a8e6-a61a4d2c4e45-5nk5re.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 100,
           style: 'normal',
@@ -101,7 +104,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Extra Light',
           data: await fetch(
-            new URL('../../../../assets/inter-extra-light.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/c3e57e57-9bc3-4aa4-9e8f-be3449318044-rjtumh.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 200,
           style: 'normal',
@@ -112,7 +118,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Light',
           data: await fetch(
-            new URL('../../../../assets/inter-light.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/903ba127-3d59-4c0d-ad78-2faa3c287517-x3r3c5.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 300,
           style: 'normal',
@@ -123,7 +132,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Regular',
           data: await fetch(
-            new URL('../../../../assets/inter-regular.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/93a8ed78-9aae-43a5-a66f-6f3e95923f56-6h2v6z.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 400,
           style: 'normal',
@@ -134,7 +146,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Medium',
           data: await fetch(
-            new URL('../../../../assets/inter-medium.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/23672c7f-ae6d-4a4e-937e-bc71e4324d72-w7jnpi.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 500,
           style: 'normal',
@@ -145,7 +160,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Semi Bold',
           data: await fetch(
-            new URL('../../../../assets/inter-semi-bold.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/e93c4078-56ee-4a0f-a8d3-82880b7391ca-j2ou90.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 600,
           style: 'normal',
@@ -156,7 +174,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Bold',
           data: await fetch(
-            new URL('../../../../assets/inter-bold.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/e88c938c-49a8-47f5-92b5-a9d63d621c73-5n8t92.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 700,
           style: 'normal',
@@ -167,7 +188,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Extra Bold',
           data: await fetch(
-            new URL('../../../../assets/inter-extra-bold.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/76e9de6f-63a6-43bc-8237-eed53baa9269-v6nn44.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 800,
           style: 'normal',
@@ -178,7 +202,10 @@ export async function GET(req: NextRequest) {
       ? ({
           name: 'Inter Black',
           data: await fetch(
-            new URL('../../../../assets/inter-black.otf', import.meta.url)
+            new URL(
+              'https://utfs.io/f/061a6245-d7c7-4bb0-8ccb-10ce431d0769-wyaxse.otf',
+              import.meta.url
+            )
           ).then((res) => res.arrayBuffer() as any),
           weight: 900,
           style: 'normal',
