61acbc736baad32d32815fe9f1fe9558c6e9cf62
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-06-22 21:55:37 -0300

    fix

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,3 @@
-import StarOnGithubButton from '@/app/(marketing)/StarOnGithubButton'
-import Footer from '@/app/(marketing)/footer'
-import { Separator } from '@/app/ui/components/Separator'
-import ValidatorInput from '@/app/ui/validator-input'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -34,35 +30,7 @@ export const metadata: Metadata = {
 export default async function Home() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-start gap-10">
-      {/* Validator */}
-      <div className="flex w-full flex-col items-center justify-center gap-2 p-4 px-8">
-        <h2 className="marketing-second-title text-balance text-center">
-          Social Card Validator
-        </h2>
-        <p className="marketing-subtitle text-balance text-center text-muted-foreground lg:px-40">
-          Check how your links look when shared. Validate that you have the
-          right metatags in place so your cards are displayed correctly.
-        </p>
-        <div className="mt-8 flex w-full items-center justify-center">
-          <ValidatorInput />
-        </div>
-      </div>
-
-      <Separator />
-
-      {/* Open Source */}
-      <div className="flex w-full flex-col items-center justify-center gap-2 p-4 px-8">
-        <h2 className="marketing-second-title text-balance text-center">
-          Open-source
-        </h2>
-        <p className="marketing-subtitle text-balance text-center text-muted-foreground">
-          Source code is available on GitHub. <br />
-          Feel free to read, review, or contribute to it however you want!
-        </p>
-        <StarOnGithubButton variant="long" />
-      </div>
-
-      <Footer />
+      sharepreviews.com
     </div>
   )
 }
